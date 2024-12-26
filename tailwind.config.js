3e8245da640c8d65d13934df1797cae0dbb9b6e1
yuta <yu.code.tech@gmail.com>
2024-12-26 23:58:44 +0900

    prettierとeslintを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./docs/*.html", "./docs/assets/script/*.js"],
+  content: ['./docs/*.html', './docs/assets/script/*.js'],
   theme: {
     extend: {},
   },
